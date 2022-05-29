fbd9f20962c22e3776e5d88ccd165e1ce96327a6
DanielTotev <dtotev1@gmail.com>
2022-05-29 15:18:29 +0300

    Updated home page

--- src/pages/HomePge.jsx
+++ src/pages/HomePge.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePge() {
-  return <div>HomePge</div>;
+  return (
+    <div class="p-5 mb-4 bg-light rounded-3 mt-4">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Rent a car</h1>
+        <p class="col-md-8 fs-4">
+          Car rental system. Admin customer can add/edit/delete caras and
+          edit/delete customers. Regular customers can see list of available
+          cars and rent them. In order to see our list of cars please{" "}
+          <Link to="/register">Register</Link> or <Link to="/login">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
