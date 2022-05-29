5e1b3c6d20e648db10cbb85177a88c48b6f791f1
DanielTotev <dtotev1@gmail.com>
2022-05-29 20:41:10 +0300

    Improved validation in login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,12 +3,19 @@ import { Row, Col, Form, Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { login } from "../util/customerUtils";
 import { useFormState } from "../util/useFormState";
+import { isEmail, notEmpty } from "../util/validators";
 
 export default function LoginPage() {
-  const [formState, handleInputChange] = useFormState({
-    email: "",
-    password: "",
-  });
+  const [formState, handleInputChange, errors, touched] = useFormState(
+    {
+      email: "",
+      password: "",
+    },
+    {
+      email: [notEmpty, isEmail],
+      password: [notEmpty],
+    }
+  );
   const [error, setError] = useState("");
   const navigate = useNavigate();
   const handleSubmit = (e) => {
@@ -44,6 +51,9 @@ export default function LoginPage() {
                 value={formState.email}
                 onChange={handleInputChange}
               />
+              {errors.email && (
+                <Form.Text style={{ color: "red" }}>{errors.email}</Form.Text>
+              )}
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
@@ -54,8 +64,23 @@ export default function LoginPage() {
                 value={formState.password}
                 onChange={handleInputChange}
               />
+              {errors.email && (
+                <Form.Text style={{ color: "red" }}>
+                  {errors.password}
+                </Form.Text>
+              )}
             </Form.Group>
-            <Button variant="primary" type="submit" style={{ float: "right" }}>
+            <Button
+              variant="primary"
+              type="submit"
+              style={{ float: "right" }}
+              disabled={
+                !(
+                  touched.length === Object.keys(formState).length &&
+                  Object.keys(errors).length === 0
+                )
+              }
+            >
               Submit
             </Button>
           </Form>
